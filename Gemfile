21a0c1833d3b1d07236b434d6bed519b206f54b1
Jakub Pavlik <severus@post.cz>
2023-05-14 18:53:19 +0200

    simple listing of psalms

--- Gemfile
+++ Gemfile
@@ -7,4 +7,5 @@ gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby, :x64_mingw]
 gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw, :x64_mingw]
 
 gem 'dotenv'
+gem 'pslm'
 gem 'rake'
--- config.rb
+++ config.rb
@@ -1,6 +1,39 @@
 # Activate and configure extensions
 # https://middlemanapp.com/advanced/configuration/#configuring-extensions
 
+require 'dotenv/load'
+require 'pslm'
+
+IN_ADIUTORIUM_PATH = ENV['IN_ADIUTORIUM_PATH'] || raise('envvar required')
+
+module Psalms
+  extend self
+
+  Psalm = Struct.new(:title, :path, :path_name, :is_canticle) do
+    def web_path
+      "/#{is_canticle ? 'kantikum' : 'zalm'}/#{path_name.sub(/zalm|kantikum_/, '')}.html"
+    end
+  end
+
+  def all
+    reader = Pslm::PslmReader.new
+
+    Dir[File.join(IN_ADIUTORIUM_PATH, 'antifonar', 'zalmy', '*.zalm')]
+      .reject {|i| i =~ /doxologie|responsorialni|pascha/ }
+      .sort
+      .collect do |i|
+      Psalm.new(
+        reader.read_str(File.read(i)).header.title,
+        i,
+        File.basename(i).sub(/\.zalm$/, ''),
+        File.basename(i).start_with?('kantikum')
+      )
+    end
+  end
+end
+
+
+
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
@@ -19,23 +52,24 @@ page '/*.txt', layout: false
 # Proxy pages
 # https://middlemanapp.com/advanced/dynamic-pages/
 
-# proxy(
-#   '/this-page-has-no-template.html',
-#   '/template-file.html',
-#   locals: {
-#     which_fake_page: 'Rendering a fake page with a local variable'
-#   },
-# )
+Psalms.all.each do |ps|
+  proxy(
+    ps.web_path,
+    '/psalm.html',
+    locals: {psalm: ps},
+    ignore: true
+  )
+end
 
 # Helpers
 # Methods defined in the helpers block are available in templates
 # https://middlemanapp.com/basics/helper-methods/
 
-# helpers do
-#   def some_helper
-#     'Helping'
-#   end
-# end
+helpers do
+  def all_psalms
+    Psalms.all
+  end
+end
 
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
