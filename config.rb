48dfda6fdfc698c256ae257f9a79dc81b8b4490e
Jakub Pavlik <severus@post.cz>
2023-05-29 15:47:58 +0200

    pointing: log file path
    to make the pointing log more useful

--- config.rb
+++ config.rb
@@ -83,6 +83,7 @@ end
 
 class PsalmMarkup
   def self.call(path)
+    STDERR.puts "pointing #{path}"
     new(Pslm::PslmReader.new.read_str(
           File.read(path).strip
             .gsub('\zalmVersUpozorneni{2}', '').gsub('\textit{', '').gsub('}', '') + # hardcoded LaTeX markup in Benedictus
